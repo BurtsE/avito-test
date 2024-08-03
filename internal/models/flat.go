bbb51044cf2148f1e0423929df347e609283c043
beus <burcev-egor@mail.ru>
2024-08-03 09:54:42 +0300

    naming changes

--- internal/models/flat.go
+++ internal/models/flat.go
@@ -1,7 +1,22 @@
 package models
 
+var (
+	OnModerate EnumRole = &onModerate{}
+	Created    EnumRole = &created{}
+	Approved   EnumRole = &approved{}
+	Declined   EnumRole = &declined{}
+)
+
 type Flat struct {
 	Id         uint32
 	Cost       uint64
 	FlatNumber uint32
+	Status     FlatStatus
 }
+
+type FlatStatus interface{ isEnumStatus() }
+
+type onModerate struct{ EnumRole }
+type created struct{ EnumRole }
+type approved struct{ EnumRole }
+type declined struct{ EnumRole }
