665a6df34274f705da83ddbefdcca4d014aec6b8
beus <burcev-egor@mail.ru>
2024-08-03 20:06:58 +0300

    internal server error fix

--- internal/router/error_responces.go
+++ internal/router/error_responces.go
@@ -6,22 +6,19 @@ import (
 	"github.com/valyala/fasthttp"
 )
 
-
 // TODO 500 code fix
-func internalServerErrorResponce(ctx *fasthttp.RequestCtx)  {
+func internalServerErrorResponce(ctx *fasthttp.RequestCtx) {
 	ctx.SetStatusCode(500)
 	ctx.Response.AppendBody(fmt.Appendf([]byte{}, `{
 		"message": %s,
-		"request_id": %s,
+		"request_id": %d,
 		"code": %d
-	}`, "что-то пошло не так", "g12ugs67gqw67yu12fgeuqwd", 12345))
+	}`, "что-то пошло не так", ctx.ID(), 500))
 }
-
 
 func invalidDataResponce(ctx *fasthttp.RequestCtx) {
 	ctx.SetStatusCode(400)
 }
-
 
 func unAuthorized(ctx *fasthttp.RequestCtx) {
 	ctx.SetStatusCode(401)
