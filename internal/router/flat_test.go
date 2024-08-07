60411d6a5b2b29a28db1f164aa64020fcfbd8373
BurtsE <burcev-egor@mail.ru>
2024-08-07 21:32:51 +0300

    some test added

---
+++ internal/router/flat_test.go
@@ -0,0 +1,67 @@
+package router
+
+import (
+	"avito-test/internal/config"
+	"avito-test/internal/models"
+	"avito-test/internal/test/mocks"
+	"context"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+	"github.com/valyala/fasthttp"
+)
+
+func Test_createFlat(t *testing.T) {
+	houseService := mocks.NewHouseService(t)
+	authService := mocks.NewAuthentificationService(t)
+	validationService := mocks.NewValidationService(t)
+	flatData := []byte(
+		`{
+  			"house_id": 12345,
+ 			"price": 10000,
+  			"rooms": 4
+		}`)
+	houseId := uint64(12345)
+	price := uint64(10000)
+	rooms := byte(4)
+	flatBuilder := models.FlatBuilder{
+		HouseId: &houseId,
+		Price:   &price,
+		Rooms:   &rooms,
+	}
+	flat := models.Flat{
+		HouseId:    houseId,
+		Price:      price,
+		RoomNumber: rooms,
+		Status:     models.OnModerate,
+	}
+	apiCtx := new(fasthttp.RequestCtx)
+	apiCtx.Request.AppendBody(flatData)
+	apiCtx.SetUserValue("role", models.UserRole)
+	ctx := context.WithValue(context.Background(), models.Role{}, apiCtx.UserValue("role"))
+	res := apiCtx.Response.Body()
+
+	expectedResponce :=
+		`{"id":0,"house_id":12345,"price":10000,"rooms":4,"status":"on moderate"}`
+	validationService.
+		On("ValidateFlatBuilderData", ctx, flatData).
+		Return(flatBuilder, nil)
+
+	houseService.
+		On("CreateFlat", ctx, flatBuilder).
+		Return(&flat, nil)
+
+	router := NewRouter(logrus.New(), &config.Config{}, houseService, validationService, authService)
+	f := &flatImpl{
+		r: router,
+	}
+	f.createFlat(apiCtx)
+	responce := apiCtx.Response.Body()
+	validationService.AssertCalled(t, "ValidateFlatBuilderData", ctx, flatData)
+	houseService.AssertCalled(t, "CreateFlat", ctx, flatBuilder)
+	if string(responce) != expectedResponce {
+		t.Fatalf("wrong result. Expected: %s\nGot: %s", expectedResponce, res)
+	}
+	validationService.AssertCalled(t, "ValidateFlatBuilderData", ctx, flatData)
+	houseService.AssertCalled(t, "CreateFlat", ctx, flatBuilder)
+}
---
+++ internal/router/house_test.go
@@ -0,0 +1,67 @@
+package router
+
+import (
+	"avito-test/internal/config"
+	"avito-test/internal/models"
+	"avito-test/internal/test/mocks"
+	"context"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+	"github.com/valyala/fasthttp"
+)
+
+func Test_getHouseData(t *testing.T) {
+	tests := []struct {
+		role   models.EnumRole
+		flats  []*models.Flat
+		result string
+	}{
+		{
+			role: models.UserRole,
+			flats: []*models.Flat{
+				{Status: models.Approved},
+			},
+			result: `[{"id":0,"house_id":0,"price":0,"rooms":0,"status":"approved"}]`,
+		},
+		{
+			role: models.ModeratorRole,
+			flats: []*models.Flat{
+				{Status: models.Approved},
+				{Status: models.OnModerate},
+				{Status: models.Created},
+			},
+			result: `[{"id":0,"house_id":0,"price":0,"rooms":0,"status":"approved"},{"id":0,"house_id":0,"price":0,"rooms":0,"status":"on moderate"},{"id":0,"house_id":0,"price":0,"rooms":0,"status":"created"}]`,
+		},
+	}
+	houseService := mocks.NewHouseService(t)
+	authService := mocks.NewAuthentificationService(t)
+	validationService := mocks.NewValidationService(t)
+	id := uint64(2)
+	h := houseImpl{
+		r: NewRouter(logrus.New(), &config.Config{}, houseService, validationService, authService),
+	}
+
+	for _, test := range tests {
+		apiCtx := new(fasthttp.RequestCtx)
+		apiCtx.SetUserValue("id", "2")
+		apiCtx.SetUserValue("role", test.role)
+		ctx := context.WithValue(context.Background(), models.Role{}, test.role)
+
+		apiCtx.SetUserValue("id", "2")
+		validationService.
+			On("ValidateHouse", ctx, id).
+			Return(nil)
+		houseService.
+			On("HouseFlats", ctx, id).
+			Return(test.flats, nil)
+		h.getHouseData(apiCtx)
+		responce := apiCtx.Response.Body()
+		validationService.AssertCalled(t, "ValidateHouse", ctx, id)
+		houseService.AssertCalled(t, "HouseFlats", ctx, id)
+		if string(responce) != test.result {
+			t.Log(responce)
+			t.Fatalf("wrong result. Expected: %s\nGot: %s", test.result, responce)
+		}
+	}
+}
